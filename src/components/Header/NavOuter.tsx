42e6ed5d753b94881b6bc6cd845da254bc0ebce8
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-08-07 00:02:57 +0200

    fix ts error

--- src/components/Header/NavOuter.tsx
+++ src/components/Header/NavOuter.tsx
@@ -1,30 +1,27 @@
 'use client'
-import React from "react"
-import { Content } from "@prismicio/client"
-import Nav from "@/components/Header/nav/index"
-import {AnimatePresence} from "framer-motion";
+import React from "react";
+import { Content } from "@prismicio/client";
+import Nav from "@/components/Header/nav/index";
+import { AnimatePresence } from "framer-motion";
 
 export type NavBarProps = {
     settings: Content.SettingsDocument;
-}
+};
 
-function NavBar({settings}: NavBarProps) {
+const NavBar: React.FC<NavBarProps> = ({ settings }) => {
     const [isActive, setIsActive] = React.useState(false);
 
     return (
         <>
             <div className="fixed right-0 m-5 z-20 w-[80px] h-[80px] rounded-full bg-primary-green cursor-pointer flex items-center justify-center"
-                 onClick={()=> {setIsActive(!isActive)}} >
-                <div className={` burger ${isActive ? 'burgerActive' : '' }`} ></div>
+                 onClick={() => { setIsActive(!isActive); }} >
+                <div className={`burger ${isActive ? 'burgerActive' : ''}`}></div>
             </div>
             <AnimatePresence mode="wait">
                 {isActive && <Nav settings={settings} />}
             </AnimatePresence>
         </>
-    )
-        ;
-}
+    );
+};
 
 export default NavBar;
-
-
--- src/components/Header/nav/index.tsx
+++ src/components/Header/nav/index.tsx
@@ -1,14 +1,12 @@
-import React, {useState} from 'react';
-import {usePathname} from "next/navigation";
-import {PrismicLink} from "@prismicio/react";
-import {NavBarProps} from "@/components/Header/NavOuter";
+import React, { useState } from 'react';
+import { usePathname } from "next/navigation";
+import { PrismicLink } from "@prismicio/react";
+import { NavBarProps } from "@/components/Header/NavOuter";
 import { motion } from 'framer-motion';
-import {menuSlide, scale, slide} from "@/components/Header/anim";
+import { menuSlide, slide } from "@/components/Header/anim";
 import Curve from "@/components/Header/Curve/index";
 
-
-function Index({settings}: {settings: NavBarProps}) {
-
+const Index: React.FC<NavBarProps> = ({ settings }) => {
     const pathname = usePathname();
     const [selectedIndicator, setSelectedIndicator] = useState(pathname);
     const isActive = selectedIndicator === pathname;
@@ -21,26 +19,27 @@ function Index({settings}: {settings: NavBarProps}) {
             exit="exit"
             className="h-screen z-10 fixed right-0 top-0 text-white bg-primary-green">
             <div className="h-full p-[100px] flex flex-col justify-between ">
-                <div className="flex flex-col text-5xl gap-3 mt-[80px]"  >
-                    {settings.data.navigation.map((item:any, index:number) => (
-                        <>
-                            <motion.div
-                                onMouseEnter={() => setSelectedIndicator(item.link.url)}
-                                custom={index}
-                                variants={slide}
-                                initial="initial"
-                                animate="enter"
-                                exit="exit"
-                                className="mb-[17px] relative flex items-center" key={item.label}>
-                                    <PrismicLink className="inline-flex min-h-11 items-center" field={item.link}>{item.label}</PrismicLink>
-                            </motion.div>
-                        </>
+                <div className="flex flex-col text-5xl gap-3 mt-[80px]">
+                    {settings.data.navigation.map((item: any, index: number) => (
+                        <motion.div
+                            key={item.label}
+                            onMouseEnter={() => setSelectedIndicator(item.link.url)}
+                            custom={index}
+                            variants={slide}
+                            initial="initial"
+                            animate="enter"
+                            exit="exit"
+                            className="mb-[17px] relative flex items-center">
+                            <PrismicLink className="inline-flex min-h-11 items-center" field={item.link}>
+                                {item.label}
+                            </PrismicLink>
+                        </motion.div>
                     ))}
                 </div>
             </div>
             <Curve />
         </motion.div>
     );
-}
+};
 
 export default Index;
