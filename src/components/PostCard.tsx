b7f209b27a9283b83605e86a47dfe0fdb2a39a27
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-08-18 14:21:20 +0200

    fix front post card !

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -12,13 +12,13 @@ export const PostCard = ({
     const { data } = post;
 
     return (
-        <div className="bg-slate-200 rounded-xl hover:[box-shadow:2px_3px_20px_0_#e3e3e3]">
+        <div className="bg-slate-200 rounded-xl hover:[box-shadow:2px_3px_20px_0_#e3e3e3] relative">
             <PrismicNextImage
                 field={data.featured_image}
                 sizes="100vw"
                 className="w-full h-[240px] rounded-xl rounded-b-none object-cover"/>
 
-            <div className="flex flex-col gap-3 p-6 relative">
+            <div className="flex flex-col gap-3 p-6">
                 <div className="flex flex-col w-full gap-1">
                     <time dateTime={dateConverter(data?.publication_date ? data.publication_date.toString() : "")} className="text-sm opacity-75 transition-all ease text-slate-700 border-b-2 w-full pb-1">
                         {dateConverter(data?.publication_date ? data.publication_date.toString() : "")}
