6fc423b17cb641869ae47b3636d7604bbf7e899c
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-10-28 14:13:56 +0100

    fix padding home

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -14,7 +14,10 @@ export default function Bounded({
 }: BoundedProps) {
   return (
     <Comp
-      className={clsx("py-14 first:pt-10 md:px-6 md:py-20 lg:py-24", className)}
+      className={clsx(
+        "px-4 py-14 first:pt-10 md:px-6 md:py-20 lg:py-24",
+        className,
+      )}
       {...restProps}
     >
       <div className="mx-auto flex h-full w-full max-w-6xl flex-col items-center">
