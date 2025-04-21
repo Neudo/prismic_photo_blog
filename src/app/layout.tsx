029e558ecc1c0c9a0d48b4022e2cdf331919e080
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2025-04-21 12:51:24 +0200

    fix: build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,17 +11,13 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import Seo from "@/components/Seo";
 import Script from "next/script";
 import { dmSans } from "@/app/ui/fonts";
-import { PostHogProvider } from "posthog-js/react";
+import { PostHogProvider } from "@/app/providers";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-  if (!posthogKey) {
-    throw new Error("Missing NEXT_PUBLIC_POSTHOG_KEY for PostHogProvider");
-  }
   return (
     <html lang="fr-FR" className={`${dmSans.variable} `}>
       <Seo />
@@ -32,7 +28,7 @@ export default function RootLayout({
           <SmoothScroller />
         </Suspense>
         <NavProvider>
-          <PostHogProvider apiKey={posthogKey}>
+          <PostHogProvider>
             <Header />
             <main>
               <div id="inner">
